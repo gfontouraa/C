fe8e496c78b2dfa64cd5cff490637d438117ba20
Gui Fontoura <britzgui@gmail.com>
2020-09-27 23:28:56 -0300

    aula 06

---
+++ aula 06/ep06.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+int numero_escolhido, resto, soma_numeros=0;
+
+do{
+    printf("Escolha um numero inteiro maior que 0: ");
+    scanf("%i", &numero_escolhido);
+
+}while(numero_escolhido < 0);
+
+for (int i = 1; i <= (numero_escolhido - 1); i++)
+{
+    resto = numero_escolhido % i;
+if (resto == 0)
+    {
+        soma_numeros = soma_numeros + i;
+    }
+}
+
+if (soma_numeros == numero_escolhido){
+    printf("\nEh um numero perfeito\n");
+}
+else {
+    printf("\nNao eh um numero perfeito\n");
+}
+
+system("pause");
+
+return 0;
+
+
+}
